98e8f8280fd3ea69b9c1a10324470f90703abde6
monaSalih <monaalobisat@gmail.com>
2021-09-01 14:24:16 +0300

    newUpdating

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -4,7 +4,7 @@ class AboutUs extends React.Component {
     render() {
         return (
             <div classNameName="mainCont">
-                <div className="row about-cols" style={{ marginLeft: "25%" }}>
+                <div className="row about-cols" style={{ marginLeft: "11%" }}>
 
                     <div className="col-md-4 wow fadeInUp" >
                         <div className="about-col">
@@ -25,6 +25,25 @@ class AboutUs extends React.Component {
                         </div>
                     </div>
 
+                    <div className="col-md-4 wow fadeInUp" >
+                        <div className="about-col">
+                            <div class="flip-card">
+                                <div class="flip-card-inner">
+                                    <div class="flip-card-front">
+                                    <img src="https://cdn.discordapp.com/attachments/880434007140085811/882564794194997288/87090765_2242164509426115_8345937203561496576_n.jpg" alt="Avatar" style={{ width: "300px", height: "300px" }} />
+                                    </div>
+                                    <div class="flip-card-back">
+                                        <div className="cardContant">
+                                        <h1>Ahmad Alrasheed</h1>
+                                        <p>Biomedical Engineering</p>
+                                        {/* <p>We love that guy</p> */}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
 
 
 
@@ -32,13 +51,13 @@ class AboutUs extends React.Component {
                         <div className="about-col">
                             <div class="flip-card">
                                 <div class="flip-card-inner">
-                                    <div class="flip-card-front">
-                                        <img src="https://cdn.discordapp.com/attachments/880434007140085811/882564794194997288/87090765_2242164509426115_8345937203561496576_n.jpg" alt="Avatar" style={{ width: "300px", height: "300px" }} />
+                                <div class="flip-card-front">
+                                        <img src="https://cdn.discordapp.com/attachments/880434007140085811/882567867885846568/me.PNG" alt="Avatar" style={{ width: "300px", height: "300px" }} />
                                     </div>
                                     <div class="flip-card-back">
                                     <div className="cardContant">
-                                        <h1>Ahmad Alrasheed</h1>
-                                        <p>Biomedical Engineering</p>
+                                        <h1>Mona Al-obisat</h1>
+                                        <p>Computer science</p>
                                         </div>
                                     </div>
                                 </div>
@@ -54,14 +73,14 @@ class AboutUs extends React.Component {
                     <div className="col-md-4 wow fadeInUp" style={{ marginLeft: "25%" }}>
                         <div className="about-col">
                             <div class="flip-card">
-                                <div class="flip-card-inner">
+                            <div class="flip-card-inner">
                                     <div class="flip-card-front">
-                                        <img src="https://cdn.discordapp.com/attachments/880434007140085811/882567867885846568/me.PNG" alt="Avatar" style={{ width: "300px", height: "300px" }} />
+                                        <img src="https://cdn.discordapp.com/attachments/880434007140085811/882565077453131816/khalid_hamedi.jpg" alt="Avatar" style={{ width: "300px", height: "300px" }} />
                                     </div>
                                     <div class="flip-card-back">
                                     <div className="cardContant">
-                                        <h1>Mona Al-obisat</h1>
-                                        <p>Computer science</p>
+                                        <h1>khalid_hamedi</h1>
+                                        <p>Management Information Systems</p>
                                         </div>
                                     </div>
                                 </div>
@@ -88,26 +107,7 @@ class AboutUs extends React.Component {
                             </div>
                         </div>
                     </div>
-
-                    
-                    <div className="col-md-4 wow fadeInUp" style={{marginTop:"5%"}}>
-                        <div className="about-col">
-                            <div class="flip-card" style={{ marginLeft: "115%" }}>
-                                <div class="flip-card-inner">
-                                    <div class="flip-card-front">
-                                        <img src="https://cdn.discordapp.com/attachments/880434007140085811/882565077453131816/khalid_hamedi.jpg" alt="Avatar" style={{ width: "300px", height: "300px" }} />
-                                    </div>
-                                    <div class="flip-card-back">
-                                    <div className="cardContant">
-                                        <h1>khalid_hamedi</h1>
-                                        <p>Management Information Systems</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
+                     
                 </div>
             </div>
 
